b1172f0c3395c0cd21ff218492b709289c660dae
GaryHsu <gary.hsu@thinkpower-info.com>
2019-01-20 18:28:15 +0800

    2019/01/20 其他音樂作品更新

--- js/general/custom/main-music.js
+++ js/general/custom/main-music.js
@@ -8,6 +8,7 @@
 	// initial tab for IE problem
 	$(".nav-tabs a:eq(0)").trigger("click");
 	$(".nav-tabs a:eq(1)").trigger("blur");
+	$(".nav-tabs a:eq(2)").trigger("blur");
 
 	resize();
 	$(window).on("resize", resize);
